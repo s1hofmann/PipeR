a4fd7e3bd9b1fbad08ad300c0af6fd4e57e1b93a
Simon Hofmann <mail@simon-hofmann.org>
2015-11-18 00:15:11 +0100

    Applied latest implementations

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,20 @@
 #include "pipeline/PipeLine.h"
 #include "preprocessing/ImageProcessingStep.h"
 #include "preprocessing/MaskGenerationStep.h"
+#include "feature_extraction/SiftDetector.h"
+#include "encoding/VladEncoder.h"
 
 int main() {
     pipe::PipeLine<int> pipeLine;
-    cv::Ptr<pipe::ImageProcessingStep> pre = new pipe::ImageProcessingStep("Image preprocessing", "I'm just a simple test", "to see if it works");
+    cv::Ptr<pipe::ImageProcessingStep> pre = new pipe::ImageProcessingStep();
     cv::Ptr<pipe::MaskGenerationStep> pre2 = new pipe::MaskGenerationStep("Mask generation", "I'm another stupid test!", "to see if it works");
+    cv::Ptr<pipe::FeatureExtractionStep> fe = new pipe::SiftDetector();
+    cv::Ptr<pipe::EncodingStep> en = new pipe::VladEncoder();
 
     pipeLine.addPreprocessingStep(pre);
     pipeLine.addPreprocessingStep(pre2);
+    pipeLine.addFeatureExtractionStep(fe);
+    pipeLine.addEncodingStep(en);
 
     pipeLine.showPipeline();
     return 0;
