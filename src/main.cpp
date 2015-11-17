4f8bf739ec8851139b0a1dabad040cb0596f083a
Simon Hofmann <mail@simon-hofmann.org>
2015-11-17 18:53:24 +0100

    First test with preprocessing pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+#include <opencv2/core/core.hpp>
+#include "pipeline/PipeLine.h"
+#include "preprocessing/ImageProcessingStep.h"
+#include "preprocessing/MaskGenerationStep.h"
+
 int main() {
+    pipe::PipeLine pipeLine;
+    cv::Ptr<pipe::ImageProcessingStep> pre = new pipe::ImageProcessingStep("Image preprocessing", "I'm just a simple test", "to see if it works");
+    cv::Ptr<pipe::MaskGenerationStep> pre2 = new pipe::MaskGenerationStep("Mask generation", "I'm another stupid test!", "to see if it works");
+
+    pipeLine.addPreprocessingStep(pre);
+    pipeLine.addPreprocessingStep(pre2);
+
+    pipeLine.showPipeline();
     return 0;
 }
