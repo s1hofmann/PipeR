99c3d5b021261a86ab62c7a88e4f0ddf73cab821
Simon Hofmann <mail@simon-hofmann.org>
2015-11-30 14:40:01 +0100

    Another test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include "pipeline/PipeLine.h"
 
-int main() {
-    pipe::PipeLine<int> pipeLine;
-    cv::Ptr<pipe::ConfigContainer> feCfg = new pipe::SiftConfigContainer();
-    cv::Ptr<pipe::FeatureExtractionStep> fe = new pipe::SiftDetector(feCfg);
+int main(int argc, char *argv[]) {
+    cv::Mat in = cv::imread(argv[1], CV_LOAD_IMAGE_UNCHANGED);
 
-    pipeLine.addFeatureExtractionStep(fe);
+    pipe::PipeLine<cv::Mat> pipeLine;
 
-    std::cout << fe->help() << std::endl;
-    std::cout << fe->usage() << std::endl;
+    cv::Ptr<pipe::VesselMaskConfig> prCfg = new pipe::VesselMaskConfig();
+    cv::Ptr<pipe::PreprocessingStep> pr = new pipe::VesselMask(prCfg, "Hurr durr, I'm a sheep!");
 
+    cv::Ptr<pipe::SiftConfigContainer> feCfg = new pipe::SiftConfigContainer();
+    cv::Ptr<pipe::SiftDetector> fe = new pipe::SiftDetector(feCfg, "hallo");
+
+    std::cout << pr->config() << std::endl;
+
+    pipeLine.addPreprocessingStep(pr);
+
+    pipeLine.train(in, cv::Mat());
     pipeLine.showPipeline();
 
     return 0;
