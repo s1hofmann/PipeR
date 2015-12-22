4414bf0f21a4add10955f34e2af3c03fc1e776e7
Simon Hofmann <mail@simon-hofmann.org>
2015-12-22 16:13:57 +0100

    Added PCA save path

--- src/dimensionality_reduction/PCAConfig.cpp
+++ src/dimensionality_reduction/PCAConfig.cpp
@@ -6,7 +6,7 @@ namespace pipe {
 
 PCAConfig::PCAConfig(int components,
                      double epsilon,
-                     bool whiten = true)
+                     bool whiten, const std::__cxx11::string &path)
     :
         ConfigContainer(),
         mComponents(components),
@@ -17,19 +17,27 @@ PCAConfig::PCAConfig(int components,
 }
 
 
-int PCAConfig::getComponents()
+int PCAConfig::getComponents() const
 {
     return this->mComponents;
 }
 
-double PCAConfig::getEpsilon()
+
+double PCAConfig::getEpsilon() const
 {
     return this->mEpsilon;
 }
 
-bool PCAConfig::getWhitening()
+
+bool PCAConfig::getWhitening() const
 {
     return this->mWhitening;
+}
+
+
+std::string PCAConfig::getPath() const
+{
+    return this->mPath;
 }
 
 
--- src/dimensionality_reduction/PCAConfig.h
+++ src/dimensionality_reduction/PCAConfig.h
@@ -8,13 +8,18 @@ namespace pipe {
 class PCAConfig : public ConfigContainer
 {
 public:
-    PCAConfig(int components, double epsilon, bool whiten);
+    PCAConfig(const int components,
+              const double epsilon,
+              const bool whiten = true,
+              const std::string &path = "./pca.yml");
 
-    int getComponents();
+    int getComponents() const;
 
-    double getEpsilon();
+    double getEpsilon() const;
 
-    bool getWhitening();
+    bool getWhitening() const;
+
+    std::string getPath() const;
 
     virtual std::string toString();
 
@@ -24,6 +29,8 @@ private:
     double mEpsilon;
 
     bool mWhitening;
+
+    std::string mPath;
 };
 
 
