50fde0d7ea5f564f3341ad9772d4df9ba35cc63a
Simon Hofmann <mail@simon-hofmann.org>
2015-12-30 14:05:30 +0100

    Added files for SGD classification

---
+++ src/ml/SGDStep.cpp
@@ -0,0 +1,45 @@
+#include "SGDStep.h"
+
+
+namespace pl {
+
+
+SGDStep::SGDStep(const cv::Ptr<ConfigContainer> config,
+                 const std::string &info)
+    :
+        MLStep(config,
+               info)
+{
+}
+
+
+SGDStep::~SGDStep()
+{
+}
+
+
+cv::Mat SGDStep::train(const cv::Mat &input, const cv::Mat &mask) const
+{
+
+}
+
+
+cv::Mat SGDStep::run(const cv::Mat &input, const cv::Mat &mask) const
+{
+
+}
+
+
+cv::Mat SGDStep::debugTrain(const cv::Mat &input, const cv::Mat &mask) const
+{
+
+}
+
+
+cv::Mat SGDStep::debugRun(const cv::Mat &input, const cv::Mat &mask) const
+{
+
+}
+
+
+}
---
+++ src/ml/SGDStep.h
@@ -0,0 +1,11 @@
+#ifndef SGDSTEP_H
+#define SGDSTEP_H
+
+
+class SGDStep : public MLStep
+{
+public:
+    SGDStep();
+};
+
+#endif // SGDSTEP_H
