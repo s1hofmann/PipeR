56fbee9d67a3bf1bef109deaec7b2e8062417a5c
Simon Hofmann <mail@simon-hofmann.org>
2016-07-21 23:48:39 +0200

    Added bounds check for resize in scalespace computation

--- src/pl/core/utils/scalespace.cpp
+++ src/pl/core/utils/scalespace.cpp
@@ -22,16 +22,25 @@ void LinearScaleSpace::compute(const cv::Mat & img,
 void GaussianScaleSpace::compute(const cv::Mat & img,
                                  std::vector<cv::Mat> & scales)
 {
-    for( int o = 0; o < mOctaves; o++ ) {
-        for( int s = 0; s < mSublevels; s++ ) {
+    for(int o = 0; o < mOctaves; ++o) {
+        for(int s = 0; s < mSublevels; ++s) {
             double f = getScaleFactor(o, s);
             double sigma = mSigmaBase * f;
             cv::Mat tmp;
             cv::GaussianBlur(img, tmp, cv::Size(), sigma);
 
-            if ( (mScaleOctave && s == 0)
-                 || mScaleSublevel){
-                 cv::resize(tmp, tmp, cv::Size(), 1.0/f, 1.0/f, cv::INTER_NEAREST);
+            if(((mScaleOctave && s) == 0) || mScaleSublevel) {
+                try {
+                    int newHeight = static_cast<int>(tmp.rows / f);
+                    int newWidth = static_cast<int>(tmp.cols / f);
+                    if(newHeight * newWidth <= 0) {
+                        info("Scalespace max. reached. Octave", o, "Sublevel:", s);
+                        return;
+                    }
+                    cv::resize(tmp, tmp, cv::Size(), 1.0/f, 1.0/f, cv::INTER_NEAREST);
+                } catch(cv::Exception) {
+                    return;
+                }
             }
 
             scales.push_back(tmp);
--- src/pl/core/utils/scalespace.h
+++ src/pl/core/utils/scalespace.h
@@ -3,7 +3,7 @@
 
 #include <vector>
 #include <opencv2/core/core.hpp>
-
+#include "../../pipeline/globals.h"
 
 namespace pl {
 
